891c2b420654a1c5ca0784903c8b89d8f8e00c8b
Jjosephi <31075794+FlavorlessQuark@users.noreply.github.com>
2023-01-12 18:20:50 -0800

    Fixed SDLX_Button crash
    Fixed a bug where SDLX_Buttons would crash if button keymaps weren't set

--- srcs/SDLX/SDLX_buttons.c
+++ srcs/SDLX/SDLX_buttons.c
@@ -67,7 +67,7 @@ void SDLX_ButtonUpdate()
 
 	while(i < 4)
 	{
-		if (currentFocus && SDLX_GetKeyState(keys[i]) == SDLX_KEYDOWN && currentFocus->neighbours[i] != NULL)
+		if (currentFocus && keys[i] != -1 && SDLX_GetKeyState(keys[i]) == SDLX_KEYDOWN && currentFocus->neighbours[i] != NULL)
 		{
 			// SDL_Log("Here %d", i);
 			currentFocus = currentFocus->neighbours[i];
