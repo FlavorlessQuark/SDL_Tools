1d261a7ab26d184e831f82b11177232c2c02d37e
FlavorlessQuark <flavorlessquark@gmail.com>
2023-06-21 21:43:47 -0700

    Fixed container space between and added utils to use containers

--- srcs/SDLX/SDLX_containers.c
+++ srcs/SDLX/SDLX_containers.c
@@ -1,4 +1,31 @@
-# include "SDLX/SDLX.h"
+# include "../../includes/SDLX/SDLX.h"
+
+
+void SDLX_ContainerPrint(SDLX_RectContainer *current,SDLX_RectContainer *parent)
+{
+	SDL_Log("Container %p | Child Of %p | Position (%d,%d) | Dimensions (%d,%d) | Container childCount %d | ElemChildCount %d",
+		current, parent,
+		current->self.boundingBox->x,
+		current->self.boundingBox->y,
+		current->self.boundingBox->w,
+		current->self.boundingBox->h,
+		current->containerCount,
+		current->elemCount
+	);
+	for (int x = 0; x < current->containerCount; x++)
+		SDLX_ContainerPrint(&current->containers[x], current);
+	for (int x = 0; x < current->elemCount; x++)
+	{
+		SDL_Log("Elem Child %d Of %p | Position (%d,%d) | Dimensions (%d,%d) ",
+		x,
+		current, parent,
+		current->elems[x].boundingBox->x,
+		current->elems[x].boundingBox->y,
+		current->elems[x].boundingBox->w,
+		current->elems[x].boundingBox->h
+		);
+	}
+}
 
 void SDLX_ContainerElemCreate(SDLX_ContainerElem *dest, SDL_Rect *boundingBox, int margin, int widthType, int heightType)
 {
@@ -23,10 +50,12 @@ void SDLX_ContainerElemCreate(SDLX_ContainerElem *dest, SDL_Rect *boundingBox, i
 void get_innerMeasurements(SDLX_RectContainer *container)
 {
 	SDLX_ContainerElem *elems;
+	SDLX_RectContainer *childCont;
 	int i;
 
 	i = 0;
 	elems = container->elems;
+	childCont = container->containers;
 	while (i < container->elemCount)
 	{
 		if (container->alignDirection == SDLX_ALIGN_VERTICAL)
@@ -39,10 +68,23 @@ void get_innerMeasurements(SDLX_RectContainer *container)
 			container->innerWidth += elems[i].boundingBox->w + elems[i].margin;
 			container->innerHeight = MAX(elems[i].boundingBox->h + elems[i].margin, container->innerHeight);
 		}
-		// SDL_Log("Inner wifdth %d Inner Hieght %d", container->innerWidth, container->innerHeight);
 		i++;
 	}
-	// SDL_Log("FINAL : Inner wifdth %d Inner Hieght %d", container->innerWidth, container->innerHeight);
+	i = 0;
+	while (i < container->containerCount)
+	{
+		if (container->alignDirection == SDLX_ALIGN_VERTICAL)
+		{
+			container->innerWidth = MAX(childCont[i].self.boundingBox->w + childCont[i].self.margin, container->innerWidth);
+			container->innerHeight += childCont[i].self.boundingBox->h + childCont[i].self.margin;
+		}
+		else
+		{
+			container->innerWidth += childCont[i].self.boundingBox->w + childCont[i].self.margin;
+			container->innerHeight = MAX(childCont[i].self.boundingBox->h + childCont[i].self.margin, container->innerHeight);
+		}
+		i++;
+	}
 }
 
 int calc_offset(SDLX_RectContainer *container, SDLX_Display *display)
@@ -66,7 +108,7 @@ int calc_offset(SDLX_RectContainer *container, SDLX_Display *display)
 	if (container->alignment == SDLX_CENTER_ALIGN)
 		offset = (direction - measurement) / 2;
 	else if (container->alignment == SDLX_SPACE_EVEN)
-		offset = (direction - measurement) / (container->elemCount + 1);
+		offset = (direction - measurement) / (container->elemCount + container->containerCount + 1);
 	else if (container->alignment == SDLX_RIGHT_ALIGN || container->alignment == SDLX_BOTTOM_ALIGN)
 		offset = direction - measurement;
 	return offset;
@@ -89,9 +131,12 @@ int calc_spacing(SDLX_RectContainer *container, SDLX_Display *display)
 		direction = container->self.boundingBox->w;
 	}
 	if (container->alignment == SDLX_SPACE_EVEN)
-		spacing = (direction - measurement) / (container->elemCount + 1);
+		spacing = (direction - measurement) / (container->elemCount + container->containerCount + 1);
 	else if (container->alignment == SDLX_SPACE_BETWEEN)
-		spacing = direction - measurement;
+	{
+		// spacing = direction - measurement;
+		spacing = (direction - measurement) / MAX(1, (container->elemCount + container->containerCount - 1));
+	}
 	return spacing;
 }
 
@@ -103,40 +148,44 @@ void position_elems( SDLX_RectContainer *container, SDLX_Display *display)
 	int x;
 	int y;
 
-	// SDL_Log("Here");
 	offset = calc_offset(container, display);
 	spacing = calc_spacing(container, display);
 
 	dir = container->alignDirection == SDLX_ALIGN_VERTICAL;
 	x = container->self.boundingBox->x + (offset * (dir ^ 1));
 	y = container->self.boundingBox->y + (offset * (dir ^ 0));
-	// SDL_Log("Spacing %d Offset %d", spacing, offset);
+	SDL_SetRenderDrawColor(SDLX_DisplayGet()->renderer, 255, 0, 0, 255);
+	for (int i = 0; i < container->containerCount; i++)
+	{
+		// Override the axis of alignment ex: if vertical aligned object has a Y set, replace it to be aligned, but don't replace the X
+		container->containers[i].self.boundingBox->x = x ;
+		container->containers[i].self.boundingBox->y = y ;
+		// container->elems[i].boundingBox->x = x + container->self.boundingBox->x;
+		// container->elems[i].boundingBox->y = y + container->self.boundingBox->y;
+		x += (container->containers[i].self.boundingBox->w + spacing) * (dir ^ 1);
+		y += (container->containers[i].self.boundingBox->h + spacing) * (dir ^ 0);
+
+		SDL_RenderDrawRect(SDLX_DisplayGet()->renderer, container->containers[i].self.boundingBox);
+	}
+	// x = container->self.boundingBox->x + (offset * (dir ^ 1));
+	// y = container->self.boundingBox->y + (offset * (dir ^ 0));
+	SDL_SetRenderDrawColor(SDLX_DisplayGet()->renderer, 255, 255, 255, 255);
 	for (int i = 0; i < container->elemCount; i++)
 	{
 		// Override the axis of alignment ex: if vertical aligned object has a Y set, replace it to be aligned, but don't replace the X
-		// SDL_Log("Box %d (%d,%d) | w: %d, h: %d",i, container->elems[i].boundingBox->x
-		// , container->elems[i].boundingBox->y
-		// , container->elems[i].boundingBox->w
-		// , container->elems[i].boundingBox->h
-		// );
-		// SDL_Log(" %d X %d Y %d | Firection %d", x, y, dir);
-		container->elems[i].boundingBox->x = x + container->self.boundingBox->x;
-		container->elems[i].boundingBox->y = y + container->self.boundingBox->y;
+
+		container->elems[i].boundingBox->x = x ;
+		container->elems[i].boundingBox->y = y ;
 		x += (container->elems[i].boundingBox->w + spacing) * (dir ^ 1);
 		y += (container->elems[i].boundingBox->h + spacing) * (dir ^ 0);
-		// SDL_Log("Box %d (%d,%d) | w: %d, h: %d",i, container->elems[i].boundingBox->x
-		// , container->elems[i].boundingBox->y
-		// , container->elems[i].boundingBox->w
-		// , container->elems[i].boundingBox->h
-		// );
-		// SDL_Log("Elem %d %d,%d", i, container->elems[i].boundingBox->x, container->elems[i].boundingBox->y);
+		SDL_RenderDrawRect(SDLX_DisplayGet()->renderer, container->elems[i].boundingBox);
 	}
 
 }
 
-// No dynamic size for now
-// We will consider the bounding box to be the total space that will be occupied. This means that when the box is too small it will not readjust
-// We can make it dynamic by first claculting the size of all the inner elements
+// No override for now, elements can only be a % of the parents element.
+// First parent element is the display
+// Therefore the first container's size is determined by the size of the display and the specified %
 void SDLX_ContainerUpdate(SDLX_RectContainer *container, SDLX_RectContainer *parent)
 {
 	int i;
@@ -148,15 +197,14 @@ void SDLX_ContainerUpdate(SDLX_RectContainer *container, SDLX_RectContainer *par
 	container->innerHeight = 0;
 	display = SDLX_DisplayGet();
 
+
 	get_innerMeasurements(container);
 	position_elems(container, display);
+
 	while (i < container->containerCount)
 	{
-		// container->containers[i].innerWidth = 0;
-		// container->containers[i].innerHeight = 0;
 		SDLX_ContainerUpdate(&container->containers[i], container);
-		// container->innerWidth += container->containers[i].innerWidth;
-		// container->innerHeight += container->containers[i].innerWidth;
 		i++;
 	}
+	SDL_SetRenderDrawColor(SDLX_DisplayGet()->renderer, 0, 0, 0, 255);
 }
---
+++ srcs/SDLX/SDLX_containers_utils.c
@@ -0,0 +1,168 @@
+# include "../../includes/SDLX/SDLX.h"
+// Container config format :
+//  1 line per object
+// Objects can be elements or containers
+// Containers are objects within which elements can be arranged
+// A container can have 0+ children and those children can be containers or elements
+// Elements are leaf node, they have no children
+//
+//  First line MUST describes a container
+//  Container: x,y,w%,h%|wType,hType,margin|alignDir,alignType
+//  Element:   x,y,w%,h%|wType,hType,margin
+//  c[amount] : container x amount
+//  e[amount] : elem x amount
+//
+// Where :
+// x , y : x and y offset in pixels
+// w% , h% : width and height, specified as a % of the respective dimension of the parent object
+// wType,hType: irrelevant for now, ignore
+// margin: margin around the object
+// alignDir : SDLX_ALIGN_VERTICAL or SDLX_ALIGN_HORIZONAL Direction of the container, the children elements will be aligned following this direction
+// alignType : How the elements in the container are spaced and positionned
+//      SDLX_RIGHT_ALIGN  = |o o _ _|
+//      SDLX_CENTER_ALIGN = |_ o o _|
+//      SDLX_LEFT_ALIGN   = |_ _ o o|
+//      SDLX_TOP_ALIGN
+//      SDLX_BOTTOM_ALIGN,
+//      SDLX_SPACE_EVEN   = |_ o _ o _|
+//      SDLX_SPACE_BETWEEN= |o _ _ _ o|
+//      SDLX_ALIGN_VERTICAL,
+//      SDLX_ALIGN_HORIZONTAL
+
+
+
+// You may use tabs to align children with their parent
+//  containers and elements will be parented to the container that came before
+int	extract_num(char *str, int *number)
+{
+	int spn;
+
+	spn = strcspn(str, NUMS);
+	*number = atoi(str + spn);
+	return spn + strspn(str + spn, NUMS);
+}
+
+char *fetch_file(char *filename, size_t *size)
+{
+	FILE	*file;
+	char 	*content;
+	file = fopen(filename, "r");
+	fseek(file, 0L , SEEK_END);
+
+	*size= ftell(file);
+	content = calloc(*size + 1, 1);
+
+	rewind(file);
+	fread(content, *size, 1, file);
+	fclose(file);
+	return content;
+}
+
+char *fill_elem(SDLX_ContainerElem *elem, SDLX_RectContainer *parent, char *line)
+{
+	int num;
+	SDLX_Display *display;
+
+	display = SDLX_DisplayGet();
+	elem->boundingBox = &elem->_boundingBox;
+	line = line + extract_num(line, &num);
+	elem->boundingBox->x = num;
+	line = line + extract_num(line, &num);
+	elem->boundingBox->y = num;
+	line = line + extract_num(line, &num);
+
+	if (line[0] == '%')
+		num = ((parent ? parent->self.boundingBox->w : display->win_w) * num ) / 100;
+	elem->boundingBox->w = num;
+
+	line = line + extract_num(line, &num);
+	if (line[0] == '%')
+		num = ((parent ? parent->self.boundingBox->h : display->win_h)  * num ) / 100;
+
+	elem->boundingBox->h = num;
+	line = line + extract_num(line, &num);
+	elem->heightTYpe = num;
+	line = line + extract_num(line, &num);
+	elem->widthTYpe = num;
+	line = line + extract_num(line, &num);
+	elem->margin = num;
+
+	return line;
+}
+
+char *fill_container(SDLX_RectContainer *container, SDLX_RectContainer *parent, char *line)
+{
+	int num;
+
+	line = fill_elem(&container->self, parent, line);
+	line = line + extract_num(line, &num);
+	container->alignDirection = num;
+	line = line + extract_num(line, &num);
+	container->alignment = num;
+
+	return line;
+}
+
+
+char *parse_object(SDLX_RectContainer *current,SDLX_RectContainer *parent, char *line)
+{
+	int count;
+
+	// SDL_Log("Start %s", line);
+	fill_container(current, parent, line);
+	line = strtok(NULL, "\n");
+
+	if (line)
+		line += strspn(line, "\t");
+	count = 0;
+	if (line && line[0] == 'c')
+	{
+		count = SDL_atoi(line + 1);
+		current->containerCount = count;
+		current->containers = SDL_calloc(count, sizeof(SDLX_RectContainer));
+		line = strtok(NULL, "\n");
+		for (int i = 0; i < count && line; i++)
+		{
+			line = parse_object(&current->containers[i], current, line);
+			if (i > 0  && current != NULL)
+			{
+				if (current->alignDirection == SDLX_ALIGN_HORIZONTAL)
+					current->containers[i].self.boundingBox->x += current->containers[i - 1].self.boundingBox->x + current->containers[i - 1].self.boundingBox->w;
+				else
+					current->containers[i].self.boundingBox->y += current->containers[i - 1].self.boundingBox->y + current->containers[i - 1].self.boundingBox->h;
+			}
+		}
+	}
+	if (line && line[0] == 'e')
+	{
+		count = SDL_atoi(line + 1);
+		current->elemCount = count;
+		current->elems = SDL_calloc(count, sizeof(SDLX_ContainerElem));
+		for (int i = 0; i < count; i++)
+		{
+			line = strtok(NULL, "\n");
+			line = fill_elem(&current->elems[i],current, line);
+		}
+		line = strtok(NULL, "\n");
+	}
+	return line;
+
+}
+
+SDLX_RectContainer *parse_UIConfig(char *filename)
+{
+	SDLX_RectContainer *parent;
+	char *file;
+	size_t size = 0;
+
+	file = fetch_file(filename, &size);
+	file = strtok(file, "\n");
+
+	parent = calloc(1, sizeof(SDLX_RectContainer));
+	parse_object(parent, NULL, file);
+	SDL_free(file);
+
+	return parent;
+}
+
+
